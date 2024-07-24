3d51206c03a46f5c167021e3cdbaa869de7a61dc
Tejiri <tejiriaustin123@gmail.com>
2024-07-24 01:44:40 -0700

    wip

---
+++ src/components/Board.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="board">
+    <Column
+        v-for="column in columns"
+        :key="column.id"
+        :column="column"
+    />
+  </div>
+</template>
+
+<script>
+import Column from '@/components/KanbanColumn.vue'
+
+export default {
+  name: 'Kanban-Board',
+  components: {
+    Column
+  },
+  data() {
+    return {
+      columns: [
+        { id: 1, title: 'To Do', tasks: [] },
+        { id: 2, title: 'In Progress', tasks: [] },
+        { id: 3, title: 'Done', tasks: [] }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/KanbanColumn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="column">
+    <h2>{{ column.title }}</h2>
+    <Task
+        v-for="task in column.tasks"
+        :key="task.id"
+        :task="task"
+    />
+    <button @click="addTask">Add Task</button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import Task from '@/components/Task.vue'
+
+
+const props = defineProps(['column'])
+
+const addTask = () => {
+  props.column.tasks.push({
+    id: Date.now().toString(), // simple way to generate unique id
+    title: 'New Task'
+  })
+}
+</script>
